253f5ca0789f8f7c8bd255f264ef3dd111dc23ce
Grayce Muthui <graycemuthui20@gmail.com>
2023-06-19 17:13:18 +0300

    Import bodyparser

--- app.js
+++ app.js
@@ -5,13 +5,41 @@
 //  path - work with file and directory paths
 // os - work with the operating system
 
-const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const routes = require("./routes");
+// express is a function
+const app = express();
+app.use(bodyParser.urlencoded());
+
+app.use("/", (req, res, next) => {
+  console.log("This always runs!");
+  next();
+});
+
+app.use("/add-product", (req, res, next) => {
+  console.log("In another middleware!");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>"
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  // need to parse the body
+  console.log(req.body);
+  res.redirect("/");
+});
+
+// use is a middleware function
+app.use("/", (req, res, next) => {
+  console.log("In the middleware");
+  // send allows us to send a response
+  res.send("<h1>Hello from Express!</h1>");
+});
 
 // the server takes the request and response objects
 // event loop - these events keep on running as long as there are event listeners registered
 // CreateServer is an event listener
-const server = http.createServer(routes);
+// const server = http.createServer(app);
 
-server.listen(3000);
+app.listen(3000);
