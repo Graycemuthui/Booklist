d6c7c9ee02d11ed5d7f0d2a96da8d6a4de117e98
Grayce Muthui <graycemuthui20@gmail.com>
2023-07-18 16:02:57 +0300

    Resolve bug

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(shopRoutes);
 
 // create a 404 page
 app.use((req, res, next) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
   console.log("404");
 });
 
